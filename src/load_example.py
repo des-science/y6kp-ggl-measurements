6eeb70f56ffd54972fc13d6e727c9bba4d78a4ff
Judit Prat <jprat@cori11.nersc.gov>
2018-04-09 09:40:30 -0700

    Updating yaml files and load example.

--- src/load_example.py
+++ src/load_example.py
@@ -4,7 +4,6 @@ sys.path.append('../../destest/')
 import destest
 
 param_file = './destest_metacal.yaml'
-print param_file
 params_mcal = yaml.load(open(param_file))
 source_mcal = destest.H5Source(params_mcal)
 selector_mcal = destest.Selector(params_mcal,source_mcal)
@@ -19,18 +18,16 @@ params_gold = yaml.load(open(param_file))
 source_gold = destest.H5Source(params_gold)
 selector_gold  = destest.Selector(params_gold,source_gold,inherit=selector_mcal)
 
-ra = selector_gold.get_col('alphawin_j2000') 
-dec = selector_gold.get_col('deltawin_j2000') 
+ra = selector_gold.get_col('ra') 
+dec = selector_gold.get_col('dec') 
 e1 = selector_mcal.get_col('e1')
 e2 = selector_mcal.get_col('e2')
 psf_e1 = selector_mcal.get_col('psf_e1')
 psf_e2 = selector_mcal.get_col('psf_e2')
 snr = selector_mcal.get_col('snr')
-psf_size = selector_mcal.get_col('psf_size')
-size_ratio = selector_mcal.get_col('size_ratio')
+size = selector_mcal.get_col('size')
 
 bpz_mean = selector_bpz.get_col('bpz_zmean_sof')
 bpz_zmc = selector_bpz.get_col('bpz_zmc_sof')
 
-
-
+calibrator = destest.MetaCalib(params_mcal,selector_mcal)
