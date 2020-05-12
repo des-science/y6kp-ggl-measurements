3717f9a7f78102028d01bda8196a8e543c319256
jpratmarti <jpratmarti@gmail.com>
2020-05-12 07:00:18 -0700

    Updating blinding scripts.

--- src/test_blinding.py
+++ src/test_blinding.py
@@ -1,14 +1,27 @@
 import os
 
-#sample = 'redmagic'
-sample = 'maglim'
+sample = 'redmagic'
+#sample = 'maglim'
+
+#scales = 'KP'
+scales = 'HOD'
 
 path_2pt_pipeline = '../../2pt_pipeline/'
 
+
 if sample == 'redmagic':
-        file_to_blind = '/global/homes/j/jprat/y3-3x2pt-methods/cosmosis/data_vectors/v0.40_fiducial.fits'
+        if scales == 'KP':
+                file_to_blind = '/global/homes/j/jprat/y3-3x2pt-methods/cosmosis/data_vectors/v0.40_fiducial.fits'
+                ini_ending = sample
+        if scales == 'HOD':
+                file_to_blind = '/global/homes/j/jprat/y3-3x2pt-methods/cosmosis/data_vectors/sim_HOD.fits'
+                ini_ending = sample + '_' + scales
+
 if sample == 'maglim':
         file_to_blind = '/global/homes/j/jprat/y3-3x2pt-methods/cosmosis/data_vectors/fiducial_maglim_cov_sourcesv040.fits'
+        ini_ending = sample
+
+print(file_to_blind)
 
 def blind_measurements():
         """
@@ -19,7 +32,8 @@ def blind_measurements():
 
         owd = os.getcwd()
         os.chdir(path_2pt_pipeline)
-        os.system("python pipeline/blind_2pt_usingcosmosis.py -i pipeline/blinding_params_%s.ini -b add -u %s"%(sample,file_to_blind))
+        print('We are using this params file: blinding_params_%s.ini'%ini_ending)
+        os.system("python pipeline/blind_2pt_usingcosmosis.py -i pipeline/blinding_params_%s.ini -b add -u %s"%(ini_ending,file_to_blind))
         os.chdir(owd)
 
 blind_measurements()
