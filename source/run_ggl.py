0bea1b038820fb1b68dfc8c48be935dded7ae1be
Georgios <georgezacharekgas@gmail.com>
2023-03-20 16:33:44 -0500

    setting up first run

--- source/run_ggl.py
+++ source/run_ggl.py
@@ -29,10 +29,10 @@ if __name__ == "__main__":
     # run code to generate measurements and covariance
     if info.NG_mode=='gt_only':
         print('Running gamma_t calculations (no Jackknife covariance)...')
-        jack_run.run_gammat()
-    elif NG_mode=='gt_and_cov':
+        run.run_gammat()
+    elif info.NG_mode=='gt_and_cov':
         print('Running gamma_t and Jackknife covariance calculations...')
-        jack_run.run_gammat_and_cov()
+        run.run_gammat_and_cov()
     else:
         errmsg = "!!!Error: Requested NG mode not implemented"
         raise Exception(errmsg)
